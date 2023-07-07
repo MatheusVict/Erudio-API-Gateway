bfcf1f7f268903575f49d9dbcf0f233172e97752
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-06 21:23:00 -0300

    basic route and header configuration

--- src/main/java/br/com/erudio/apigateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/br/com/erudio/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,4 +1,30 @@
 package br.com.erudio.apigateway.configuration;
 
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
 public class ApiGatewayConfiguration {
+
+  @Bean
+  public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+    Function<PredicateSpec, Buildable<Route>> function =
+            predicateSpec -> predicateSpec.path("/get")
+                    .filters(gatewayFilterSpec -> gatewayFilterSpec
+                            .addRequestHeader("Hello", "world")
+                            .addRequestParameter("hello", "world")
+                    )
+                    .uri("http://httpbin.org:80");
+    return builder
+            .routes()
+            .route(function)
+            .build();
+  }
 }
